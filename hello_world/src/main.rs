fe765f1f199f4c63a12d1c7667e573f046eed66b
th11jr@aol.com <th11jr@aol.com>
2024-01-02 17:57:23 -0600

    Adding Comments

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    //Just started going through the book. This is one of the first excercises
+    //that teaches what macros are.
     println!("Hello World!");
     println!("I'm a Rustacean!");
 }
