667fdb64cd80ee6e2bbd7ab53ea5435ac0e4d14d
th11jr@aol.com <th11jr@aol.com>
2024-01-07 09:38:47 -0600

    Changing structs to practice making my unique structures!

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,18 +1,36 @@
 #[derive(Debug)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    fn new(x: f64, y:f64) -> Self {
+        Point{ x, y }
+    }
+}
+
+#[derive(Debug)]
+struct Circle {
+    center: Point,
+    radius: f64,
+}
+
+impl Circle {
+    fn new(center: Point, radius: f64) -> Self {
+        Circle{ center, radius }
+    }
+
+    fn area(&self) -> f64 {
+        self.radius.powf(2.0) * std::f64::consts::PI
+    }
 }
 
 fn main() {
-    let user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
+    let center = Point::new(0.0, 0.0);
+    let circ = Circle::new(center, 2.0);
 
-    println!("user1 looks like this: {:?}", user1);
+    println!("The center of the circle is: {:?}", circ.center);
+    println!("The radius of the circle is: {:?}", circ.radius);
+    println!("The area of the circle is: {:?}", circ.area());
 }
